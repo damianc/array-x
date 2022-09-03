a9291580ac411ad661089543502c5d2f6690bcb8
damianc <damianczapiewski@gmail.com>
2022-09-03 16:12:11 +0200

    fix impls

--- src/find-min.js
+++ src/find-min.js
@@ -1,4 +1,6 @@
 export default function findMin(mapper = null) {
+  if (this.length === 0) return -1;
   if (!mapper) return Math.min(...this);
-  return this.reduce((acc,curr)=>mapper(curr)<mapper(acc)?curr:acc);
+
+  return this.reduce((acc, curr)=> mapper(curr) < mapper(acc) ? curr : acc);
 }
--- src/min.js
+++ src/min.js
@@ -3,6 +3,9 @@ export default function min(mapper = null) {
     return mapper ? mapper(x) : x;
   }
 
+  if (this.length === 0) return -1;
+  if (this.length === 1) return 0;
+
   return this.reduce((acc, curr, idx) => {
     const currMin = idx === 1 ? val(acc) : acc;
     return Math.min(val(curr), currMin);
