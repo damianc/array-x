ac97c5eab627652cf3bdabd5eb20c3834471baf4
damianc <damianczapiewski@gmail.com>
2022-09-07 20:57:18 +0200

    import extractObject() to utils

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,11 @@
 import buildIndex from './build-index';
+import extractObject from './extract-object';
 import reduce from './reduce';
 import val from './val';
 
 export {
   buildIndex,
+  extractObject,
   reduce,
   val
 };
